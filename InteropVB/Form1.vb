67107c64279d6fa3ead2d8b82200e9ce95828e18
AXEL ANTONIO GARZA MARTINEZ <garza_axel@uadec.edu.mx>
2023-08-27 03:25:15 -0600

    Agregar archivos de proyecto.

---
+++ InteropVB/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub btnWord_Click(sender As Object, e As EventArgs) Handles btnWord.Click
+
+        Dim Word = New Microsoft.Office.Interop.Word.Application
+        Dim wordApp = Word.Application()
+
+        Dim ruta, dato As String
+
+        If (SaveFileDialog1.ShowDialog() <> DialogResult.OK) Then
+            Return
+        End If
+
+        ruta = SaveFileDialog1.FileName
+        wordApp.Visible = True
+
+        wordApp.Documents.Add()
+        dato = txtDato.Text
+
+        wordApp.Selection.TypeText(dato)
+        wordApp.ActiveDocument.SaveAs(ruta)
+
+    End Sub
+
+    Private Sub btnExcel_Click(sender As Object, e As EventArgs) Handles btnExcel.Click
+
+        Dim Excel = New Microsoft.Office.Interop.Excel.Application
+        Dim excelApp = Excel.Application()
+
+        Dim ruta, dato As String
+
+        If (SaveFileDialog1.ShowDialog() <> DialogResult.OK) Then
+            Return
+        End If
+
+        ruta = SaveFileDialog1.FileName
+        excelApp.Visible = True
+
+        Dim workbook = excelApp.Workbooks.Add()
+        Dim worksheet = workbook.ActiveSheet
+
+        worksheet.Cell(1, 1) = txtDato.Text
+
+        workbook.SaveAs(ruta)
+    End Sub
+End Class
